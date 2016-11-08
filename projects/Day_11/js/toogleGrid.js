578d928b99d67f9075bf5f72fd39b5127a1d92ea
mlee9179 <lmy9179@gmail.com>
2016-11-08 19:42:24 +0900

    day 11 포지셔닝 레이아웃 테크닉

---
+++ projects/Day_11/js/toogleGrid.js
@@ -0,0 +1,9 @@
+(function(global){
+  'use strict';
+  var body = document.body;
+  document.onkeydown = function(event) {
+    if ( event.shiftKey && event.keyCode === 71 ) {
+      body.classList.toggle('show-grid');
+    }
+  };
+})(this);
